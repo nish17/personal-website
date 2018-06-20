361d5ddffd1011604c9ec848367b522c13c200a8
Nimesh Solanki <nimesh3536@gmail.com>
2018-06-20 12:35:02 +0530

    compile code

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import {
-  render
-} from 'react-dom';
-import Webpage from '../components/main.jsx';
-render( < Webpage / > , document.getElementById('app'));
+import React from "react";
+import { render } from "react-dom";
+import Webpage from "../components/main.jsx";
+render(<Webpage />, document.getElementById("app"));
