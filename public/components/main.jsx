24471d3379fec88f83e97644932277bb27f2a47c
Nimesh Solanki <nimesh3536@gmail.com>
2018-06-27 20:46:45 +0530

    so far so good

--- public/components/main.jsx
+++ public/components/main.jsx
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
-import {
-  render
-} from 'react-dom';
-
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "../css/style1.css";
 const Webpage = () => (
   <div>
     <main>
       <div id="landing">
         <div id="landing-text">
           <div id="landing-text-inner">
-            <h1 > Coming soon </h1>
-            <h2 > Website Under Development </h2>
-            <a href="https://www.github.com/nish17" target="_blank" className="btn" id="view-work" > View Work </a>
+            <h1> Coming soon </h1>
+            <h2> Website Under Development </h2>
+            <a
+              href="https://www.github.com/nish17"
+              target="_blank"
+              className="btn"
+              id="view-work"
+            >
+              {" "}
+              View Work{" "}
+            </a>
           </div>
         </div>
         <div id="landing-image"> </div>
@@ -20,12 +26,23 @@ const Webpage = () => (
 
     <footer>
       <h3> Get in Touch </h3>
-      <p> Email:<strong>  nimesh3536 @gmail.com </strong> </p>
-      <p> Phone:<strong> +91 - 9033309720 </strong> </p>
+      <p>
+        {" "}
+        Email:<strong> nimesh3536 @gmail.com </strong>{" "}
+      </p>
+      <p>
+        {" "}
+        Phone:<strong> +91 - 9033309720 </strong>{" "}
+      </p>
     </footer>
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-      crossOrigin="anonymous" > </script>
-    <script src="js/main.js" > </script>
+    <script
+      src="http://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossOrigin="anonymous"
+    >
+      {" "}
+    </script>
+    <script src="js/main.jsx"> </script>
   </div>
 );
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Webpage from "../components/main.jsx";
-render(<Webpage />, document.getElementById("app"));
+import page1 from "../components/page1.jsx";
+
+class Home extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Webpage} />
+          <Route path="/resume" component={page1} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+render(<Home />, document.getElementById("app"));
